6529e2177184d8358277774eba25d4713b13fd2a
Fernando Tapia <fernandotapia7@gmail.com>
2026-08-09 10:18:01 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "airescare-cache-v44";
+const CACHE_NAME = "airescare-cache-v45";
 
 // Los scripts de Firebase se cargan desde el servidor de Google
 // (gstatic.com), no desde este mismo sitio — por eso hay que guardarlos
